76c5fea1d5d3f772313975933e736478b6466c57
Charlieriveraa <charlierivera541@gmail.com>
2024-07-02 15:46:48 +0800

    done up to video

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+var slider = tns({
+    container: ".testimonial__slider",
+    items: 3,
+    speed: 200,
+    gutter: 100,
+    slideBy: "page",
+    autoplay: true,
+    navPosition: "bottom",
+    containerControls: "#controls",
+    prevButton: ".prev",
+    nextButton: ".next",
+  });
